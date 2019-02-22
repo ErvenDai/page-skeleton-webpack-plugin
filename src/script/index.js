794a91fa93c960fff69747ba7243a75261b6afaa
dailuwen <dailuwen@meituan.com>
2019-02-22 17:13:02 +0800

    feat(script): 优化伪类元素的渲染效果

--- src/script/index.js
+++ src/script/index.js
@@ -239,32 +239,22 @@ var Skeleton = (function (exports) {
     }
   }
 
-  function pseudosHandler({ ele, hasBefore, hasAfter }, { color, shape, shapeOpposite }) {
+  function pseudosHandler({ ele, hasBefore, hasAfter }, { shape, shapeOpposite }) {
     const finalShape = shapeOpposite.indexOf(ele) > -1 ? getOppositeShape(shape) : shape;
     const PSEUDO_CLASS = `${CLASS_NAME_PREFEX}pseudo`;
-    const PSEUDO_RECT_CLASS = `${CLASS_NAME_PREFEX}pseudo-rect`;
-    const PSEUDO_CIRCLE_CLASS = `${CLASS_NAME_PREFEX}pseudo-circle`;
 
     const rules = {
       [`.${PSEUDO_CLASS}::before, .${PSEUDO_CLASS}::after`]: `{
-      background: ${color} !important;
-      background-image: none !important;
       color: transparent !important;
       border-color: transparent !important;
     }`,
-      [`.${PSEUDO_RECT_CLASS}::before, .${PSEUDO_RECT_CLASS}::after`]: `{
-      border-radius: 0 !important;
-    }`,
-      [`.${PSEUDO_CIRCLE_CLASS}::before, .${PSEUDO_CIRCLE_CLASS}::after`]: `{
-      border-radius: 50% !important;
-    }`
     };
 
     Object.keys(rules).forEach(key => {
       addStyle(key, rules[key]);
     });
 
-    addClassName(ele, [PSEUDO_CLASS, finalShape === 'circle' ? PSEUDO_CIRCLE_CLASS : PSEUDO_RECT_CLASS]);
+    addClassName(ele, [PSEUDO_CLASS]);
   }
 
   function svgHandler(ele, { color, shape, shapeOpposite }, cssUnit, decimal) {
@@ -733,18 +723,38 @@ var Skeleton = (function (exports) {
     }
   }
 
+  const getHtmlAttrStr = (root) => {
+    const style = root.style;
+    const dataset = root.dataset;
+    const className = root.className;
+    const styleStr = style.fontSize ? `style="font-size: ${style.fontSize}"` : '';
+    const datasetStr = dataset ? Object.keys(dataset).map(key => `dara-${key}="${dataset[key]}"`).join(' ') : '';
+    const classNameStr = className ? `class="${className}"` : '';
+    return `${styleStr} ${classNameStr} ${datasetStr}`
+  };
+
   function getHtmlAndStyle() {
     const root = document.documentElement;
+    const metaStr = Array.from(document.querySelectorAll('meta')).map(item => item.outerHTML).join(' ');
+    const htmlAttrStr = getHtmlAttrStr(root);
+    const bodyFontSize = document.body.style.fontSize;
+    const bodyStyleStr = bodyFontSize ? `style="font-size=${bodyFontSize};` : '';
     const rawHtml = root.outerHTML;
-    const styles = Array.from($$('style')).map(style => style.innerHTML || style.innerText);
-    Array.from($$(AFTER_REMOVE_TAGS.join(','))).forEach(ele => removeElement(ele));
     // fix html parser can not handle `<div ubt-click=3659 ubt-data="{&quot;restaurant_id&quot;:1236835}" >`
     // need replace `&quot;` into `'`
     const cleanedHtml = document.body.innerHTML.replace(/&quot;/g, "'");
+    const styles = Array.from($$('style')).map(style => style.innerHTML || style.innerText);
+    Array.from($$(AFTER_REMOVE_TAGS.join(','))).forEach(ele => removeElement(ele));
     return {
       rawHtml,
       styles,
-      cleanedHtml
+      cleanedHtml,
+      htmlInfo: {
+        htmlAttrStr,
+        bodyStyleStr,
+        metaStr,
+        a: document.querySelectorAll('meta')
+      }
     }
   }
 
